8fa45dd287a763c688f037cced40a8ebb7f95fe0
Mehfuz Hossain <mehfuz@gmail.com>
2017-03-17 00:34:34 -0700

    fixes

--- apiai.ts
+++ apiai.ts
@@ -24,7 +24,7 @@ export class ApiAi {
                 }
             }
         }
-        return args.message.text;
+        return "";
   }
 
 }
--- flickr-request.ts
+++ flickr-request.ts
@@ -4,26 +4,31 @@ import request = require('request');
 export class FlickrRequest {
   
   send(text, success, err){
-        let url = util.format("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=%s&text=%s&page=1&format=json&nojsoncallback=1&sort=relevance", process.env.API_KEY, encodeURIComponent(text));
+        if (text.length > 0){
+            let url = util.format("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=%s&text=%s&page=1&format=json&nojsoncallback=1&sort=relevance", process.env.API_KEY, encodeURIComponent(text));
 
-        request.get(url, (error, response, body)=>{
-            if (error){
-                throw error;
-            }
+            request.get(url, (error, response, body)=>{
+                if (error){
+                    throw error;
+                }
 
-            let photos = JSON.parse(body).photos;
+                let photos = JSON.parse(body).photos;
 
-            if (photos && photos.photo.length > 0){
-                let photo = photos.photo[0];
-                let url = util.format("http://farm%s.staticflickr.com/%s/%s_%s.jpg", photo.farm, photo.server, photo.id, photo.secret);
-                
-                success({
-                    url : url
-                });
-            } else {
-                err(util.format("Sorry could not find anything for \"%s\"", text));
-            }
-        });
+                if (photos && photos.photo.length > 0){
+                    let photo = photos.photo[0];
+                    let url = util.format("http://farm%s.staticflickr.com/%s/%s_%s.jpg", photo.farm, photo.server, photo.id, photo.secret);
+                    
+                    success({
+                        url : url
+                    });
+                } else {
+                    err(util.format("Sorry could not find anything for \"%s\"", text));
+                }
+            });
+         }
+         else {
+              success({});
+         }
   }
 
 }
