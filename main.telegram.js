eab651f3fe22a684151edadad376a6f0c89c5a78
Mehfuz Hossain <mehfuz@gmail.com>
2017-03-31 14:38:15 -0700

    fixes

--- main.telegram.js
+++ main.telegram.js
@@ -16,24 +16,31 @@ export default class Bot {
 	}
 
 	execute(cb){
-		new ApiAi().extract(this.args).then((text)=>{
-			new FlickrRequest().send(text, (result)=>{
-					let message = {
-							type: "photo",
-							payload: {
-									photo: result.url
-							}
-					};
-					cb(message);
-			}, (err)=>{
-					cb({
-						text : err
-					})
-			});
-		}, (err)=>{
-			cb({
-				text : err
-			})
-		});
+    let text = this.args.message.text;
+    if (text.match(/^\/start+/i)){
+      return cb({
+        text : "Hello! I am a \"flickr bot\". You can ask me questions like \"Where is San Francisco?\" or \"Who is Steve Jobs?\" and I can show you photo of that location or person."
+      });
+    } else {
+      new ApiAi().extract(this.args).then((text)=>{
+        new FlickrRequest().send(text, (result)=>{
+            let message = {
+                type: "photo",
+                payload: {
+                    photo: result.url
+                }
+            };
+            cb(message);
+        }, (err)=>{
+            cb({
+              text : err
+            })
+        });
+      }, (err)=>{
+        cb({
+          text : err
+        })
+      });
+    }
 	}
 }
